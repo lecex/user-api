c910fa3cdebbfb807496eda50c3295e21f983495
bigrocs <bigrocs@qq.com>
2020-02-22 13:24:27 +0800

    自动提交 git 代码

--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/go-redis/redis v6.15.7+incompatible
 	github.com/gogo/protobuf v1.3.1
 	github.com/golang/protobuf v1.3.3
-	github.com/lecex/core v1.0.5
+	github.com/lecex/core v1.0.6
 	github.com/lecex/user v1.0.1
 	github.com/micro/go-micro/v2 v2.1.0
 )
--- vendor/github.com/lecex/core/client/client.go
+++ vendor/github.com/lecex/core/client/client.go
@@ -4,7 +4,6 @@ import (
 	"context"
 
 	"github.com/micro/go-micro/v2/client"
-	cli "github.com/micro/go-micro/v2/client/grpc"
 	"github.com/micro/go-micro/v2/config/cmd"
 )
 
@@ -14,7 +13,6 @@ func init() {
 
 // Call 使用默认客户端对服务进行同步调用
 func Call(ctx context.Context, service string, endpoint string, req interface{}, rsp interface{}, opts ...client.CallOption) error {
-	client.DefaultClient = cli.NewClient()
 	request := client.NewRequest(service, endpoint, req)
 	err := client.Call(ctx, request, rsp, opts...)
 	return err

2b1fec20cefbc8406fa807b8ac915d1d6ba3b411
bigrocs <bigrocs@qq.com>
2020-06-12 16:53:31 +0800

    增加健康检测时返回服务器时间

--- handler/health.go
+++ handler/health.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"context"
+	"time"
 
 	pb "github.com/lecex/user-api/proto/health"
 )
@@ -13,5 +14,6 @@ type Health struct {
 // Health 用户是否存在
 func (srv *Health) Health(ctx context.Context, req *pb.Request, res *pb.Response) (err error) {
 	res.Valid = true
+	res.Time = time.Now().Format("2006-01-02 15:04:05")
 	return nil
 }

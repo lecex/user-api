56bc4d42f9f0c754002e9fe83d3f00e2852f16bc
bigrocs <bigrocs@qq.com>
2021-06-06 16:40:04 +0800

    自动提交 git 代码

--- config/config.go
+++ config/config.go
@@ -15,7 +15,7 @@ import (
 //	Description // 权限解释
 var Conf config.Config = config.Config{
 	Name:    env.Getenv("MICRO_API_NAMESPACE", "go.micro.api.") + "user-api",
-	Version: "v1.3.1",
+	Version: "v1.3.3",
 	Service: map[string]string{
 		"user": env.Getenv("USER_SERVICE", "go.micro.srv.user"),
 	},
